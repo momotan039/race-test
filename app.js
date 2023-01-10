aaf49926ec40f828d1a993bd90bb1fb35d904feb
FaridBash <farid.bashiti@gmail.com>
2023-01-10 13:47:03 +0200

    pick2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function pickRunner(){
     let arr=[];
     // Returns a random integer from 0 to 9:
     while(arr.length<3){
-        let num=Math.floor(Math.random() * 3)+1;
+        let num=Math.floor(Math.random() * 10)+1;
         if(arr.includes(num)==false){
             arr.push(num);
         }
