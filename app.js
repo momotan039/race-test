b25a3997cfcf90db05fb04ba2ac8d3afce428a58
Yous3f <yasilah4@gmail.com>
2023-01-10 13:49:18 +0200

    yousefPlaceRacer

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const figlet = require("figlet");
+
 console.log("ASD")
+
+function placeRunner(arrNames, arrayNums) {
+  arrNames = ['farid', 'yousef', 'taha'];
+  arrayNums = [10, 30, 20]; //10 20 30 
+  let newNums=arrayNums.sort();
+  
+}
