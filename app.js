c201dff3aa398a2b6bf4eaec2e3a893d4d3b6b48
MOHAMMED <momotaha039@gmail.com>
2023-01-10 13:24:30 +0200

    last

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("ASD")
+const players=["tahani","sara","ali","shibly","ahmad","mousa","rami","tala","hamdi","talal"]
+const selected=[];
+function startRace(){
+    //Yousef
+    i=2;
+while(i>=0){
+random
+}
+}
+
+startRace();
+
